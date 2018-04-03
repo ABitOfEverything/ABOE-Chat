a01feb7705d12c1ebeed6406ca1c66f52d59dc63
Gerber Lóránt Viktor <glorantq@gmail.com>
2018-04-03 21:36:06 +0200

    FE does a R E T A R D A T I O N

--- src/main/java/me/glorantq/aboe/chat/server/permissions/ForgeEssentialsPermissionProvider.java
+++ src/main/java/me/glorantq/aboe/chat/server/permissions/ForgeEssentialsPermissionProvider.java
@@ -3,8 +3,12 @@ package me.glorantq.aboe.chat.server.permissions;
 import com.forgeessentials.api.APIRegistry;
 import com.forgeessentials.api.UserIdent;
 import com.forgeessentials.api.permissions.FEPermissions;
+import com.forgeessentials.api.permissions.GroupEntry;
+import com.forgeessentials.api.permissions.Zone;
 import com.forgeessentials.chat.ModuleChat;
 import net.minecraft.entity.player.EntityPlayer;
+
+import java.util.Set;
 
 public class ForgeEssentialsPermissionProvider implements PermissionProvider {
 
@@ -20,29 +24,44 @@ public class ForgeEssentialsPermissionProvider implements PermissionProvider {
 
     @Override
     public String getPrefix(EntityPlayer player) {
-        String prefix = ModuleChat.getPlayerPrefixSuffix(UserIdent.get(player), false);
-        if(prefix == null || prefix.trim().length() == 0) {
-            prefix = APIRegistry.perms.getGroupPermissionProperty(APIRegistry.perms.getPrimaryGroup(UserIdent.get(player)), FEPermissions.PREFIX);
-        }
-
-        if(prefix == null) {
-            return "";
-        }
-
-        return prefix;
+        return searchPrefixSuffix(player, false);
     }
 
     @Override
     public String getSuffix(EntityPlayer player) {
-        String suffix = ModuleChat.getPlayerPrefixSuffix(UserIdent.get(player), true);
-        if(suffix == null || suffix.trim().length() == 0) {
-            suffix = APIRegistry.perms.getGroupPermissionProperty(APIRegistry.perms.getPrimaryGroup(UserIdent.get(player)), FEPermissions.PREFIX);
+        return searchPrefixSuffix(player, true);
+    }
+
+    private String searchPrefixSuffix(EntityPlayer player, boolean isSuffix) {
+        UserIdent userIdent = UserIdent.get(player);
+        Zone serverZone = APIRegistry.perms.getServerZone();
+
+        String fix = ModuleChat.getPlayerPrefixSuffix(userIdent, isSuffix);
+        if(fix != null && !fix.isEmpty()) {
+            System.out.println("player fix " + fix);
+            return fix;
         }
 
-        if(suffix == null) {
+        int highestPriorityFix = Integer.MIN_VALUE;
+
+        Set<GroupEntry> groupEntries = APIRegistry.perms.getPlayerGroups(userIdent);
+
+        for(GroupEntry entry : groupEntries) {
+            String groupFix = serverZone.getGroupPermission(entry.getGroup(), isSuffix ? FEPermissions.SUFFIX : FEPermissions.PREFIX);
+            if(groupFix != null && !groupFix.isEmpty() && entry.getPriority() > highestPriorityFix) {
+                fix = groupFix;
+
+                System.out.println("new group " + entry.getGroup() + " p " + entry.getPriority() + " f " + fix);
+
+                highestPriorityFix = entry.getPriority();
+            }
+        }
+
+        if(fix == null) {
+            System.out.println("null fix");
             return "";
         }
 
-        return suffix;
+        return fix;
     }
 }
